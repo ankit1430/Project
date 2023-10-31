4a03f49e1eb5db5dffa424bec8ccacddf1c31b45
Ankit Thakur <ankitthakur1430@gmail.com>
2023-10-31 23:25:56 +0530

    Project first commit

---
+++ routes/review.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const {
+  validateReview,
+  isLoggedIn,
+  isreviewAuthor,
+} = require("../middleware.js");
+const reviewController = require("../controllers/reviews.js");
+const review = require("../models/review.js");
+
+//Reviews
+//POST Route
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(reviewController.createReview)
+);
+
+//Delete Review Route
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isreviewAuthor,
+  wrapAsync(reviewController.destroyReview)
+);
+
+module.exports = router;
